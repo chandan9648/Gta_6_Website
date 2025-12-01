c0ad5e65c8029befdcf0079fdf30afecff2fac7e
Chandan Chaudhary <chandankkumar156@gmail.com>
2025-12-02 00:20:05 +0530

    fix: reduce title font size for improved readability in App component

--- src/App.jsx
+++ src/App.jsx
@@ -88,11 +88,11 @@ const App = () => {
               <img className='absolute top-0 left-0 w-full h-full object-cover' src="./bg.png" alt="bg" />
 
                  <div className="text text-white flex flex-col gap-3 absolute top-20 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
-                   <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
-                   <h1 className="text-[12rem] leading-none ml-20">theft</h1>
-                   <h1 className="text-[12rem] leading-none -ml-40">auto</h1>
+                   <h1 className="text-[10rem] leading-none -ml-40">grand</h1>
+                   <h1 className="text-[10rem] leading-none ml-20">theft</h1>
+                   <h1 className="text-[10rem] leading-none -ml-40">auto</h1>
                 </div>
-                
+
               <img className='absolute -bottom-[55%] left-1/2 -translate-x-1/2 scale-[0.7] ' src="./girlbg.png" alt="girlbg" />
             </div>
             <div className='btmbar text-white absolute w-full bottom-0 left-0 py-12 px-10 bg-gradient-to-t from-black to-transparent'>
